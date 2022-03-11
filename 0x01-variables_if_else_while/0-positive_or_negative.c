f5d78af604464e96fe4838b2f80e35e585466187
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-11 12:25:04 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,9 @@
 /* more headers goes there */
 /**
 * main - Determine if a random number is positive ,negative or zero.
+* 
 * Return: Always 0 (success)
-**/
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
