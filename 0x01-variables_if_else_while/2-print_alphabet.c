fbe8cda049aff47be40f45528ebab93cc0b7d41a
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-12 11:33:01 -0800

    printing alphabeticaly the small laters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Alphabet game
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = "a";
+	for (; ch <= "z"; ch++)
+	{
+		putchar(ch);
+	}	
+	return (0);
+	{
