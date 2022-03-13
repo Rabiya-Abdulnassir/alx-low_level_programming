776e0fe13938b314972ec424c89e1467e0839ac5
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-12 22:04:32 -0800

    dispalying lower and upper case latter

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Lower and Upper case laters
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = "a" ; ch <= "z" ; ch++)
+	{
+		putchar(ch);
+	}
+	 for (ch = "a" ; ch <= "z" ; ch++)
+	 {
+		 putchar(ch);
+	 }
+	 putchar('\n');
+	 return (0);
+}
+
