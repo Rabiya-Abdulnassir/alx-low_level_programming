9e88b3652460eb1eff03666742fb00870e8a908f
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-13 00:38:52 -0800

    print numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,13 @@
 
 int main(void)
 {
-	int i;
 
-	for (i = '0' ; i <= '9' ; i++)
-	{
-		putchar(i);
-	}
-	putchar('\n');
-	return (0);
+int i;
+
+for (i = '0' ; i <= '9' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
 }
