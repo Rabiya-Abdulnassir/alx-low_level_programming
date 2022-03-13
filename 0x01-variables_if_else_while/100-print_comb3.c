61bdcd5a8e9a3850278fd676fcef6c54e8b3f6bd
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-13 05:51:48 -0700

    The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,13 @@ for (i = '0' ; i <= '9' ; i++)
 	{
 		if (i != j && i <= j)
 		{
-			if (i != '8' && j != '9')
-			{
+#if (i != '8' && j != '9')
+#			{
 				 putchar(i);
 			         putchar(j);
                                  putchar(',');
                                  putchar(' ');
-			}
+#}
 		}
 	}
 }
