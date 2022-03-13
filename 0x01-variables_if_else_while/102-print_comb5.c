a9c4c3d53954ec7fe05dbde8dcc0423c63301f7d
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-13 09:56:36 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,13 +22,14 @@ int main(void)
 			 	putchar((j/10)+'0');
 			        putchar((j%10)+'0');
 			  if (i != 98 || j != 99)
-			}
+			  {
 				putchar(',');
 				putchar(' ');
-			}
+			  }
 			}
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
