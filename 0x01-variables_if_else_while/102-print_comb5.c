d456b5062754c04b5a7c03bbe78d17b52a28919b
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-13 08:44:39 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - Software is eating the World
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int r;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		for (j = '0' ; j <= '9' ; j++)
+		{
+			for (k = '0' ; k <= '9' ; k++)
+			{
+				for (r = '1' ; r <= '9' ;)
+				{
+					if (i = j && k != r)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(r);
+						if (i != '9')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
