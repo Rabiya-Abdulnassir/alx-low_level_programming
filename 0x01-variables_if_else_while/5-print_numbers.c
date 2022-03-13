ae2bf15d2d8ff7af8b5ff4434d1d1734497412a6
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-12 23:31:39 -0800

    printing single digit number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - displaying number
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		printf("\n"; i);
+	}
+
+	return (0);
+}
