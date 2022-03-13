2789468de4c8131cfea17d210d8557373212c427
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-13 01:39:04 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Patience, persistence and perspiration make an unbeatabel
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+	    	putchar(i);
+		putcahr(',');
+		putchar('\n');
+	}
+	putchar('\n');
+	return (0);
+}
