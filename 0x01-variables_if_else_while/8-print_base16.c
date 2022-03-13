8a3e68559b8cadd9dfb70a770e5902b665a5862f
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-13 01:29:06 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@ int main(void)
 char ch;
 int i;
 
-for (i = 0 ; i <= 9 ; i++)
+for (i = '0'; i <= '9' ; i++)
 {
-        putchar(i);
+putchar(i);
 }
 for (ch = 'a' ; ch <= 'f' ; ch++)
-{
-        putchar(ch);
+{       
+putchar(ch);
 }
 putchar('\n');
 return (0);
