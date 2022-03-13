8a9139aef2ca16020edcc5b97dae3a6b34a694b2
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-13 03:42:58 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,14 +6,17 @@
  */
 
 int main(void)
-{	
+{
 int i;
 
-for (i = '0' ; i < '9' ; i++)
+for (i = '0' ; i <= '9' ; i++)
 {
+	if (i != 9)
+	{
 	putchar(i);
 	putchar(',');
 	putchar(' ');
+	}
 }
 putchar('\n');
 return (0);
