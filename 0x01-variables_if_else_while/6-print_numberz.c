9ff82a0e7c751ef7b57aa0a43accfbe930b8df3c
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-12 23:51:20 -0800

    printing single digit number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.c>
+/*
+ * main - printing numbers
+ *
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for  (i = 0 ; i <= 9 ; i++)
+	{
+		int putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
