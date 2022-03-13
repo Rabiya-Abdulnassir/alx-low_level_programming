c1d69e4bfcfb7229ee25987097d7ecdbbb6483fa
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-12 22:37:08 -0800

    printing selected alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - printing alphabet
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+		putchar(ch);
+		
+	}
+return (0);
+}
