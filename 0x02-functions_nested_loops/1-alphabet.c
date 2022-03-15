6a1da860a1a2b830c29cde4900282d77a50276a2
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-15 08:22:11 -0700

    when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mian - displaying 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+	return (0);
+}
