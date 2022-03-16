98f544f9928d4ca1c7964f5405a3a30193457214
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 08:19:15 -0700

    function which print every minutes days

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,27 +6,20 @@
  */
 void jack_bauer(void)
 {
-	int minutes_tens, minutes_ones, second_ones, second_tens, minutes_max;
+	int minutes_tens, minutes_ones, second_ones, second_tens;
 
-		for (minutes_tens = '0' ; minutes <= 4 ; minutes++)
+		for (minutes_tens = 1 ; minutes <= 24 ; minutes++)
 		{
-			for (minutes_ones = 0 ; minutes_ones <= 4 ; minutes_onres++)
+			for (minutes_ones = 1 ; minutes_ones <= 60 ; minutes_ones++)
 			{
-				for (second_tens = 0 ; second_tens <= 5 ; second_tens++)
-				{
-					for (second_ones = 0 ; second_ones <= 9 ; second_ones++)
-					{
-						if (minute_ones !=4 && secon_tens != 5)
-						{
-					_putchar(minutes + '0');
-					_putchar(minutes + '0');
+				
+						
+					_putchar((minutes_tens / 10) + '0');
+					_putchar((minutes_tens % 10) + '0');
 					_putchar(':'); 
-					_putchar(seconds + '0');
-					_putchar(seconds + '0');
+					_putchar((minutes_ones / 10) + '0');
+					_putchar((minutes_ones % 10)+ '0');
 					_putchar('\n');
-						}
-					}
-				}
 			}
 		}
 }
