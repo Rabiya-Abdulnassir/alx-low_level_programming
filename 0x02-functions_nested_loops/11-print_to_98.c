81dec70b2e8204f520afdb7a2c748a8dd5af0103
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 11:29:12 -0700

    function for printing all integer numbers from n t0 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,29 +6,19 @@
  */
 void print_to_98(int n)
 {
-        int i;
-        int j;
+	if (n < 98)
 
-        for (i = 0 ; i <= 99 ; i++)
-        {
-                for (j = 0 ; j <= 99 ; j++)
-                {
-                        if (i != j && i < j)
-                        {
-                                putchar((i / 10) + '0');
-                                putchar((i % 10) + '0');
-                                putchar(' ');
-                                putchar((j / 10) + '0');
-                                putchar((j % 10) + '0');
-                                if (i != 98 || j != 99)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
+	for (n = n ; n < 99 ; n++)
+	{
+		_putchar(n + '0');
+		_putchar(',');
+	}
+	else
 
-        putchar('\n');
+	for (n = n ; n > 99 ; n--)
+	{
+		_putchar(n + '0');
+		_putchar('\n');
+	
+	}
 }
-
