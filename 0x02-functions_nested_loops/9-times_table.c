899275a58cd231b4d3340572fa5fae00bfb1e2eb
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 10:21:24 -0700

    function to print 9 time tabel

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * time_tabel - print the 9 time tabel
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			k = (i*j);
+
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (k < 10)
+			{
+				_putchar(',');
+                                _putchar(' ');
+                                _putchar(' ');
+                                _putchar((k % 10) + '0');
+			}
+			else
+			{
+				_putchar(',');
+                                _putchar(' ');
+                                _putchar((k / 10) +'0');
+                                _putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+
+
+
+
