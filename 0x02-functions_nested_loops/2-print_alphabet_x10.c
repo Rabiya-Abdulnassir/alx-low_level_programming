632436c9577f1e25495cfe5d2cd1f06a534db4f1
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 01:33:00 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,13 @@
  **/
 void print_alphabet_x10(void)
 {
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{ 
-		char ch;
+	char ch;
 
-			for (ch = 'a' ; ch <= 'z' ; ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		char  ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
 			{
 				_putchar(ch);
 			}
