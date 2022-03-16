3c830fa4214f7a579cb19ff2438552cb3d37033e
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 04:46:26 -0700

    Function which checks absolute value  of integer  numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - print the absolute of integer
+ * @n: integer number
+ *
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
+
