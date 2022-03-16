7731b483d3b39730371841370dd70c43d256f552
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 13:32:08 -0700

    function for printing l natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,23 @@ void print_to_98(int n)
 {
 	if (n < 99)
 	{
-	for (n = n ; n < 99 ; n++)
+	for (n = n ; n <= 98 ; n++)
 	{
 	         printf("%d", n);
 		 if (n != 98)
 		 {
-		 printf(",");
+		 printf(", ");
 		 }
 	}
 	}
 	else
 	{
-	for (n = n ; n > 99 ; n--)
+	for (n = n ; n >= 98; n--)
 	{
 		 printf("%d", n)
 		if (n != 98)
 		{
-		printf(",");
+		printf(", ");
 		}
 	}
 	printf("\n");
