ca335543ac0b29adfef140fc9eb58e46a1cecf1b
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 07:22:25 -0700

    function which print every minutes of the truck

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,17 @@ void jack_bauer(void)
 {
 	int minutes, seconds;
 
-		for (minutes = 0 ; minutes <= 30 ; minutes++)
+		for (minutes = '0' ; minutes <= '04' ; minutes++)
 		{
 			for (seconds = 0 ; seconds <= 59 ; seconds++)
 			{
 				if (minutes != 4 && seconds != 59)
 				{
-					_putchar(minutes);
-					_putchar(minutes);
+					_putchar(minutes + '0');
+					_putchar(minutes + '0');
 					_putchar(':'); 
-					_putchar(seconds);
-					_putchar(seconds);
+					_putchar(seconds + '0');
+					_putchar(seconds + '0');
 					_putchar('\n');
 				}
 			}
