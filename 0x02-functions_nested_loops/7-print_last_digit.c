ec6d31ddf40ad3fedd434da201724a71d188d221
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 05:36:26 -0700

    Function which print every minits of the truck

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *
+ * @n: last digit number
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	if (n % 10 )
+
+
+
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * void - printing mintis of the track
+ *
+ * Return: no value 
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 23 ; i++)
+	{
+		for (j = 0 ; j <= 60 ; j++)
+		{
+			_putchar((i / 10) = 0);
+			_putchat((i % 10) = 0);
+			_putchar(':');
+			_putchar((j / 10) = 0);
+                        _putchat((j % 10) = 0);
+			_putchar('\n');
+		}
+		 _putchar('\n');
+	}
+}
+
