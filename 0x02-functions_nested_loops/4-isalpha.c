624fe7cc0a3e66d67335a2622afcad3007f1c8de
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 04:04:20 -0700

    Function which checks latter, lower caseor upercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - is print alphabet
+ *
+ * @c: ASIIC code
+ *
+ * Return: 1 is letter, lowercase or upper
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' ; && c <= 'z'|| c >= 'A' ; && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
