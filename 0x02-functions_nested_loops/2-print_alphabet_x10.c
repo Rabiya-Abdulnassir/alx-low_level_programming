74de9b36c5e8569351e316483337b7ab5df25739
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 01:04:37 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - no return value
+ *
+ * Return: Always 0 (success)
+ **/
+void print_alphabet_x10(void)
+{
+        char ch = 'a';
+
+        for (; ch <= 'z' ; ch++)
+        {
+		for (; ch <= 'z' ; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
