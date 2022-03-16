0e8052a069ff81daceb1b50a4341d8e767ebb0f1
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 06:52:29 -0700

    function which print the lats digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,7 @@ int print_last_digit(int n)
 	int last;
 
 	if (n < 0 )
-	{
-		last = -1 * (n % 10);
-	}
-	_putchar(last + '0');
+		last = -1 * (n % 10)
+	_putchar('0' + last);
 	 return (last);
 }
