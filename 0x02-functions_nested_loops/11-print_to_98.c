8f7bee95e1788759bec75591bfbe880d4716ddda
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-16 13:47:08 -0700

    function for printing l natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - print all natural numbers 
+ * print_to_98 - print all natural numbers
  * @n: Number
  *
  * Return: ALways 0
@@ -10,12 +10,12 @@ void print_to_98(int n)
 {
 	if (n < 99)
 	{
-	for (n = n ; n <= 98 ; n++)
-	{
-	         printf("%d", n);
-		 if (n != 98)
+		for (n = n ; n <= 98 ; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
 		 {
-		 printf(", ");
+			 printf(", ");
 		 }
 	}
 	printf("\n");
