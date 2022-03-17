2bbe4dc3af62ca7c2973809c73119555c077cf08
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-17 12:45:36 -0700

    function for printing  integer from 0to9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "mian.h"
+/**
+ * print_number - number from 0 to 9
+ *
+ * Return: always none
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ;i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
