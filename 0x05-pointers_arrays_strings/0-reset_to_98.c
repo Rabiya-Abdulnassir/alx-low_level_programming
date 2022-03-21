2c84af090b9b2694759130224a48ca18a76cbc52
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-21 11:50:41 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-* reset_to_98 - update the value 
-* 
+* reset_to_98 - update the value
 * @n: pointer
 * Return: ALways 0
 */
@@ -12,5 +11,5 @@ int p = 402;
 n = &p;
 _putchar('n');
 *n = 98;
- _putchar('n');
+_putchar('n');
 }
