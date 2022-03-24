9dbece89aa842f85f4da654bb66d9ed3f9f0a59e
Rabiya-Abdulnassir <tufahramz@gmail.com>
2022-03-24 07:35:19 -0700

    function that concatinate two string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,26 @@
 #include "main.h"
 /**
-* *_strcat - concatinates two sring 
-* @dest : character
-* @src: character
-* Return: Always dest
-*/
-
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
 char *_strcat(char *dest, char *src)
 {
-int count, count2;
-for (count = 0 ; *(dest + count) != '\0' ; count++)
-for (count2 = 0 ; count2 >= 0 ; count2++)
-{
-*(dest + conut) = *(src + count2);
-if ( *(src + count2) == '\0')
-break;
-count++
-}
-return (dest);
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
 }
